99d8e8d5667421e2a63573c46ecb32b68335acb1
Mario Vizcaino <mariovizcaino@MacBook-Pro-de-Mario.local>
2017-11-15 10:17:04 -0500

    fixed bacground jobs errors

--- app/jobs/file_analyzer_job.rb
+++ app/jobs/file_analyzer_job.rb
@@ -7,9 +7,8 @@ class FileAnalyzerJob < ApplicationJob
     # perform the file analysis
     csv = CSV.read(file_path)
     csv.each do |row|
-      sleep 5 # simulate heavy processing
+      #sleep 5 # simulate heavy processing
       file_analysis.update(analyzed_rows: file_analysis.analyzed_rows + 1)
-      Pusher.trigger("file-analysis-#{file_analysis.id}", 'update', file_analysis.as_json)      
     end
     file_analysis.update(status: :finished)    
   end
