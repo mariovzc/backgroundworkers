e53062fe1b5fef1e62cb6e4c1a206786fb055c04
Mario Vizcaino <mariovizcaino@MacBook-Pro-de-Mario.local>
2017-11-15 09:57:58 -0500

    fixed some bugs

--- app/controllers/file_analyses_controller.rb
+++ app/controllers/file_analyses_controller.rb
@@ -5,7 +5,7 @@ class FileAnalysesController < ApplicationController
     file = params[:file]
 
     file_analysis = FileAnalysis.create(filename: file.original_filename)
-    FileAnalyzerjob.perform_later(file.temp_file.path, file_analysis)
+    FileAnalyzerJob.perform_later(file.tempfile.path, file_analysis)
     redirect_to file_analysis
   end
 
--- app/jobs/file_analyzer_job.rb
+++ app/jobs/file_analyzer_job.rb
@@ -5,10 +5,11 @@ class FileAnalyzerJob < ApplicationJob
     puts 'Iniciando job.....'
     file_analysis.update(status: :processing)
     # perform the file analysis
-    csv = CSV.new(file.tempfile)
+    csv = CSV.read(file_path)
     csv.each do |row|
       sleep 5 # simulate heavy processing
       file_analysis.update(analyzed_rows: file_analysis.analyzed_rows + 1)
+      Pusher.trigger("file-analysis-#{file_analysis.id}", 'update', file_analysis.as_json)      
     end
     file_analysis.update(status: :finished)    
   end
